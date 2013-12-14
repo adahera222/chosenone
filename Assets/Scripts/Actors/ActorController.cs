3bd9eae17546a04c2ceed84a674405bcb0d70ed3
Stephan Hoevelbrinks <stephan.hoevelbrinks@craftinglegends.com>
2013-12-14 12:30:46 +0100

    added first version of focus trigger

--- Assets/Scripts/Actors/ActorController.cs
+++ Assets/Scripts/Actors/ActorController.cs
@@ -4,14 +4,79 @@ using System.Collections.Generic;
 
 public class ActorController : MonoBehaviour {
 
+    // ================================================================================
+    //  public
+    // --------------------------------------------------------------------------------
+
     public Actor actor { get; set; }
 
-    void Start() {
+    public ActorController target = null;
+    public GameObject displayObject;
 
+    public bool directionRight = true;
+
+    public FocusManager focus = null;
+
+    // ================================================================================
+    //  private
+    // --------------------------------------------------------------------------------
+
+    private FocusManager focusRight = null;
+    private FocusManager focusLeft = null;
+
+    // ================================================================================
+    //  Unity methods
+    // --------------------------------------------------------------------------------
+
+    void Awake()
+    {
+        focusRight = transform.Find("focusRight").GetComponent<FocusManager>();
+        focusLeft = transform.Find("focusLeft").GetComponent<FocusManager>();
+
+        UpdateFocusDirection();
+    }
+
+    void Start() {
+        GetTarget();
     }
 
     void Update() {
 
+    }
+
+    // ================================================================================
+    //  public methods
+    // --------------------------------------------------------------------------------
+
+    public void flipDirection()
+    {
+        Transform t = displayObject.transform;
+        t.localScale = new Vector3(t.localScale.x * -1.0f, t.localScale.y, t.localScale.z);
+        UpdateFocusDirection();
+    }
+
+    // ================================================================================
+    //  private methods
+    // --------------------------------------------------------------------------------
+
+    private void GetTarget()
+    {
+        if (actor.faction != Actor.Faction.Player)
+        {
+            target = GameMaster.Instance.player;
+        }
+    }
+
+    private void UpdateFocusDirection()
+    {
+        if (directionRight)
+        {
+            focus = focusRight;
+        }
+        else
+        {
+            focus = focusLeft;
+        }
     }
 
 }
---
+++ Assets/Scripts/Actors/FocusManager.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FocusManager : MonoBehaviour {
+
+    private List<ActorController> actors = new List<ActorController>();
+
+    public ActorController focus = null;
+
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("enter");
+
+        ActorController otherActor = other.gameObject.GetComponent<ActorController>();
+
+        if (otherActor != null && !actors.Contains(otherActor))
+        {
+            if (focus == null)
+            {
+                focus = otherActor;
+            }
+
+            actors.Add(otherActor);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("exit");
+
+        ActorController otherActor = other.gameObject.GetComponent<ActorController>();
+
+        if (otherActor != null && actors.Contains(otherActor))
+        {
+            actors.Remove(otherActor);
+
+            if (focus == otherActor)
+            {
+                focus = null;
+
+                if (actors.Count > 0)
+                {
+                    focus = actors[0];
+                }
+            }
+        }
+    }
+
+}
